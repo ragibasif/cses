dd96cddb47259a2f005ccb84639380ec0bea6247
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-03-01 23:32:36 -0500

    {{UPDATE}} Sat Mar  1 23:32:36 2025

--- template/main.cpp
+++ template/main.cpp
@@ -1,24 +1,25 @@
-#include <iostream> // cout, cin, endl - Input/Output
-#include <vector> // vector - dynamic array
-#include <string> // string
 #include <algorithm> // sort, binary_search, lower_bound, upper_bound
-#include <cassert> // assert - for debugging
-#include <unordered_map> // unordered_map - hash map
-#include <unordered_set> // unordered_set - hash set
-#include <queue> // queue, priority_queue
-#include <stack> // stack
-#include <set> // set, multiset - (non-hashed, tree) ordered set
-#include <map> // map, multimap - (non-hashed, tree) ordered key-value pairs
 #include <bitset> // bitset - for binary data
-#include <utility> // pair
-#include <cmath> // sqrt, pow, abs
+#include <cassert> // assert - for debugging
 #include <climits> //INT_MAX, INT_MIN
+#include <cmath> // sqrt, pow, abs
 #include <cstdint> // int64_t, uint64_t, etc. - fixed-width integer types
 #include <functional> // greater, less - function objects and operations
+#include <iostream> // cout, cin, endl - Input/Output
 #include <iterator> // iterators and related items
 #include <numeric> // accumulate, gcd, lcm - numeric operations
+#include <map> // map, multimap - (non-hashed, tree) ordered key-value pairs
+#include <queue> // queue, priority_queue
+#include <set> // set, multiset - (non-hashed, tree) ordered set
+#include <stack> // stack
+#include <string> // string
+#include <tuple>
+#include <unordered_map> // unordered_map - hash map
+#include <unordered_set> // unordered_set - hash set
+#include <utility> // pair
+#include <vector> // vector - dynamic array
+
 using namespace std; // removes the need to use std::
-void fastIO();
 void solve();
 
 
@@ -30,10 +31,15 @@ void solve() {
 
 int main() {
 
-    int t;
-    t = 1;
-    /*cin >> t;*/
-    while (t--) {
+    /*Fast I/O*/
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int testcases;
+    testcases = 1;
+    /*cin >> testcases;*/
+    while (testcases--) {
         solve();
         cout << "\n";
     }
@@ -42,8 +48,3 @@ int main() {
     return 0;
 }
 
-void fastIO() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-}
